307df112df17494c2b46730caeae93d12cba0570
Brandon Ngo <brandonngo56@gmail.com>
2025-03-30 15:30:05 -0400

    Store sessions in Prisma session store

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const passport = require("./middlewares/passport");
+const prisma = require("./db");
 const indexRouter = require("./routes/indexRouter");
 const folderRouter = require("./routes/folderRouter");
 const fileRouter = require("./routes/fileRouter");
@@ -17,11 +19,23 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
+    store: new PrismaSessionStore(
+      prisma,
+      {
+        checkPeriod: 2 * 60 * 1000, // ms optional and default is 2 mins
+        dbRecordIdIsSessionId: true, // use session ID as database record ID
+        dbRecordIdFunction: undefined, // can also use function to generate record IDs
+        logger: console, // logger optional and default is no logging
+      }
+    ),
     secret: "my-secret-key",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false },
-  }),
+    cookie: {
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24 // 24 hours
+    },
+  })
 );
 app.use(passport.session());
 
